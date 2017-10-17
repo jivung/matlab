85197e83891fe74dc594a8cf46af33b96b010c17
Michael Jivung <michael.jivung@gmail.com>
2017-10-17 14:58:39 +0200

    implemented finnfemfel

--- bildanalys/finnfemfel.m
+++ bildanalys/finnfemfel.m
@@ -1,4 +1,39 @@
 function finnfemfel
+    
+    % Load in the images
     load('/Users/michaeljivung/Desktop/femfel.mat')
-    imshowpair(femfel1, femfel2)
+    
+    % Set parameters
+    rad = 6;
+    threshold = 2300;
+    
+    % Pad the images with 3 pixels at each side
+    image1 = double(padarray(femfel1, [rad rad], 0, 'both'));
+    image2 = double(padarray(femfel2, [rad rad], 0, 'both'));
+    
+    orgSize = size(femfel1);
+    
+    res = zeros(orgSize(1), orgSize(2));
+    res2 = zeros(orgSize(1), orgSize(2));
+    
+    % Compare images 
+    for x = rad+1:orgSize(2)+rad
+        for y = rad+1:orgSize(1)+rad
+            window1 = image1(y-rad:y+rad, x-rad:x+rad, :);
+            window2 = image2(y-rad:y+rad, x-rad:x+rad, :);
+            diff = sum(sum(abs(mean(window1)-mean(window2))));
+            res2(y, x) = diff;
+            if diff > threshold
+                res(y, x) = 255;
+            end
+        end
+    end
+
+    % Output result
+    figure(1)
+    imagesc(res)
+    
+    figure(2)
+    imagesc(res2)
+    
 end
