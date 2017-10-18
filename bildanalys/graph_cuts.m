973a43810ef3add2fe7cb32b22e5cc0d321cf5d9
Michael Jivung <michael.jivung@gmail.com>
2017-10-18 17:44:35 +0200

    work on graph_cuts

--- bildanalys/graph_cuts.m
+++ bildanalys/graph_cuts.m
@@ -1,6 +1,12 @@
 function graph_cuts
 
     load heart_data.mat
+    
+    mean_background = mean(background_values);
+    std_background = std(background_values);
+    
+    mean_chamber = mean(chamber_values);
+    std_chamber = std(chamber_values);
 
 end
 
